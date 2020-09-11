a52f5a354c64d90d5d8050d8439d9a07fca1d55c
Pedro Peña <pedro@makedit.com>
2020-09-11 06:36:00 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='openwisp2',
+    version='0.1',
+    packages=['openwisp2'],
+    url='',
+    license='',
+    author='Pedro Peña',
+    author_email='pedro@makedit.com',
+    description='Makedit Openwisp2'
+)
